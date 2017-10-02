7352be1128e2c234327586e495baf6a764fccfc7
Andrew Kuykendall <Andrew Kuykendall@DESKTOP-1S7K67G>
2017-10-02 18:30:02 -0500

    Add project files.

---
+++ Tea Time Project/Main Form.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.txtPounds = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblTotalCost = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(95, 44)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrice.TabIndex = 0
+        '
+        'txtPounds
+        '
+        Me.txtPounds.Location = New System.Drawing.Point(95, 73)
+        Me.txtPounds.Name = "txtPounds"
+        Me.txtPounds.Size = New System.Drawing.Size(100, 20)
+        Me.txtPounds.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(55, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Price:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(43, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Pounds:"
+        '
+        'lblTotalCost
+        '
+        Me.lblTotalCost.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotalCost.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalCost.Location = New System.Drawing.Point(95, 149)
+        Me.lblTotalCost.Name = "lblTotalCost"
+        Me.lblTotalCost.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotalCost.TabIndex = 4
+        Me.lblTotalCost.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(158, 219)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(58, 219)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 6
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 155)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Total Price:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblTotalCost)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPounds)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Tea Time Company"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents txtPounds As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblTotalCost As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Tea Time Project/Main Form.vb
@@ -0,0 +1,64 @@
+﻿' Project Name: Tea Time Company
+' Purpose : To calculate if shipping is charged on an order or not
+'Programmer: Andrew Kuykendall on 10/01/2017 - Row 2
+
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' declare variables and constants
+        Const decShipping As Decimal = 15
+        Dim decPounds As Decimal
+        Dim decPrice As Decimal
+        Dim decTotalCost As Decimal
+        Dim dlgButton As DialogResult
+        Decimal.TryParse(txtPounds.Text, decPounds)
+        Decimal.TryParse(txtPrice.Text, decPrice)
+
+        ' calculate Cost of Tea
+        decTotalCost = decPrice * decPounds
+
+        ' Determine if charging shipping 
+        dlgButton = MessageBox.Show("Should the customer be charged a $15 shipping fee?", "Shipping Charge", MessageBoxButtons.YesNo, MessageBoxIcon.Information)
+
+        If dlgButton = DialogResult.Yes Then
+
+            decTotalCost = decShipping + decTotalCost
+
+        Else
+            decTotalCost = decTotalCost
+
+        End If
+        ' Display formatting
+        lblTotalCost.Text = decTotalCost.ToString("C2")
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Dim dlgExit As DialogResult
+        dlgExit = MessageBox.Show("Would you like to exit?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+
+        If dlgExit = DialogResult.Yes Then
+            Me.Close()
+
+        End If
+
+    End Sub
+
+    Private Sub txtPounds_Enter(sender As Object, e As EventArgs) Handles txtPounds.Enter
+        txtPounds.SelectAll()
+
+    End Sub
+
+    Private Sub txtPounds_TextChanged(sender As Object, e As EventArgs) Handles txtPounds.TextChanged, txtPrice.TextChanged
+        ' clear total amount
+        lblTotalCost.Text = String.Empty
+
+    End Sub
+
+    Private Sub txtPrice_Enter(sender As Object, e As EventArgs) Handles txtPrice.Enter
+        txtPrice.SelectAll()
+    End Sub
+End Class
